7be80f4e0d1bff5bf10ae917beb5120c778aa151
DouyaQki <douyaqki@gmail.com>
2022-07-06 10:48:09 -0300

    freecodecamp projects

--- front_end_development_libraries/drum_machine/index.jsx
+++ front_end_development_libraries/drum_machine/index.jsx
@@ -31,9 +31,7 @@ class App extends React.Component {
       document
         .getElementById(keyboardPressed)
         .play()
-        .catch(() => {
-          console.clear()
-        })
+        .catch(() => console.clear())
     }
 
     const btnAnimation = () => {
@@ -50,108 +48,158 @@ class App extends React.Component {
 
     if (e.keyCode === keyboard[keyboardPressed]) {
       btnAnimation()
+
+      const $drumText = document.getElementById('typeOfDrum')
+      $drumText.innerText = e.key.toUpperCase()
     }
   }
 
   onClickKey(e) {
+    const $drumText = document.getElementById('typeOfDrum')
+    const $drumData = document.getElementById(e.target.id)
+
+    $drumText.innerText = $drumData.innerText
+
     const $btnSound = document.getElementById(e.target.innerText)
-    $btnSound.play().catch(() => {
-      console.clear()
-    })
+    $btnSound
+      .play()
+      .catch(() => console.clear())
   }
 
   render() {
     {
       this.keyboardPressed
     }
+
     return (
       <div id='drum-machine'>
-        <article className='drum-title'>
+        <header className='drum-title'>
           <img src='assets/drum_icon.png' alt='drum icon' />
-          <h2>DRUM MACHINE</h2>
-        </article>
+          <h2>Drum Machine</h2>
+        </header>
 
         <div id='display'>
-          <section>
-            <button className='drum-pad' id='audio-q' onClick={this.onClickKey}>
-              Q
-              <audio
-                src='https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-                className='clip'
-                id='Q'
-              ></audio>
-            </button>
-            <button className='drum-pad' id='audio-w' onClick={this.onClickKey}>
-              W
-              <audio
-                src='https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
-                className='clip'
-                id='W'
-              ></audio>
-            </button>
-            <button className='drum-pad' id='audio-e' onClick={this.onClickKey}>
-              E
-              <audio
-                src='https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-                className='clip'
-                id='E'
-              ></audio>
-            </button>
-          </section>
+          <article>
+            <h2 id='typeOfDrum'></h2>
 
-          <section>
-            <button className='drum-pad' id='audio-a' onClick={this.onClickKey}>
-              A
-              <audio
-                src='https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
-                className='clip'
-                id='A'
-              ></audio>
-            </button>
-            <button className='drum-pad' id='audio-s' onClick={this.onClickKey}>
-              S
-              <audio
-                src='https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
-                className='clip'
-                id='S'
-              ></audio>
-            </button>
-            <button className='drum-pad' id='audio-d' onClick={this.onClickKey}>
-              D
-              <audio
-                src='https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
-                className='clip'
-                id='D'
-              ></audio>
-            </button>
-          </section>
+            <section>
+              <button
+                className='drum-pad'
+                id='audio-q'
+                onClick={this.onClickKey}
+                data-drum_q='drum Q'
+              >
+                Q
+                <audio
+                  src='https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+                  className='clip'
+                  id='Q'
+                ></audio>
+              </button>
+              <button
+                className='drum-pad'
+                id='audio-w'
+                onClick={this.onClickKey}
+              >
+                W
+                <audio
+                  src='https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+                  className='clip'
+                  id='W'
+                ></audio>
+              </button>
+              <button
+                className='drum-pad'
+                id='audio-e'
+                onClick={this.onClickKey}
+              >
+                E
+                <audio
+                  src='https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+                  className='clip'
+                  id='E'
+                ></audio>
+              </button>
+            </section>
 
-          <section>
-            <button className='drum-pad' id='audio-z' onClick={this.onClickKey}>
-              Z
-              <audio
-                src='https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
-                className='clip'
-                id='Z'
-              ></audio>
-            </button>
-            <button className='drum-pad' id='audio-x' onClick={this.onClickKey}>
-              X
-              <audio
-                src='https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
-                className='clip'
-                id='X'
-              ></audio>
-            </button>
-            <button className='drum-pad' id='audio-c' onClick={this.onClickKey}>
-              C
-              <audio
-                src='https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-                className='clip'
-                id='C'
-              ></audio>
-            </button>
-          </section>
+            <section>
+              <button
+                className='drum-pad'
+                id='audio-a'
+                onClick={this.onClickKey}
+              >
+                A
+                <audio
+                  src='https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+                  className='clip'
+                  id='A'
+                ></audio>
+              </button>
+              <button
+                className='drum-pad'
+                id='audio-s'
+                onClick={this.onClickKey}
+              >
+                S
+                <audio
+                  src='https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+                  className='clip'
+                  id='S'
+                ></audio>
+              </button>
+              <button
+                className='drum-pad'
+                id='audio-d'
+                onClick={this.onClickKey}
+              >
+                D
+                <audio
+                  src='https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+                  className='clip'
+                  id='D'
+                ></audio>
+              </button>
+            </section>
+
+            <section>
+              <button
+                className='drum-pad'
+                id='audio-z'
+                onClick={this.onClickKey}
+              >
+                Z
+                <audio
+                  src='https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+                  className='clip'
+                  id='Z'
+                ></audio>
+              </button>
+              <button
+                className='drum-pad'
+                id='audio-x'
+                onClick={this.onClickKey}
+              >
+                X
+                <audio
+                  src='https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+                  className='clip'
+                  id='X'
+                ></audio>
+              </button>
+              <button
+                className='drum-pad'
+                id='audio-c'
+                onClick={this.onClickKey}
+              >
+                C
+                <audio
+                  src='https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+                  className='clip'
+                  id='C'
+                ></audio>
+              </button>
+            </section>
+          </article>
         </div>
       </div>
     )
