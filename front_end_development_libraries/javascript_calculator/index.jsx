aac9aae9094e7b59f7010d6e612714eeb3478e2d
DouyaQki <douyaqki@gmail.com>
2022-07-14 15:50:41 -0300

    freecodecamp projects

--- front_end_development_libraries/javascript_calculator/index.jsx
+++ front_end_development_libraries/javascript_calculator/index.jsx
@@ -1,106 +1,188 @@
+const calcData = [
+  { id: 'clear', value: 'AC' },
+  { id: 'divide', value: '/' },
+  { id: 'multiply', value: 'x' },
+  { id: 'seven', value: 7 },
+  { id: 'eight', value: 8 },
+  { id: 'nine', value: 9 },
+  { id: 'subtract', value: '-' },
+  { id: 'four', value: 4 },
+  { id: 'five', value: 5 },
+  { id: 'six', value: 6 },
+  { id: 'add', value: '+' },
+  { id: 'one', value: 1 },
+  { id: 'two', value: 2 },
+  { id: 'three', value: 3 },
+  { id: 'equals', value: '=' },
+  { id: 'zero', value: 0 },
+  { id: 'decimal', value: '.' },
+]
+
+const operators = ['AC', '/', 'x', '+', '-', '=']
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const Display = ({ input, output }) => (
+  <div className='output'>
+    <span className='result'>{output}</span>
+    <span id='display' className='input'>
+      {input}
+    </span>
+  </div>
+)
+
+const Key = ({ keyData: { id, value }, handleInput }) => (
+  <button id={id} onClick={() => handleInput(value)}>
+    {value}
+  </button>
+)
+
+const Keyboard = ({ handleInput }) => (
+  <div className='keys'>
+    {calcData.map((key) => (
+      <Key key={key.id} keyData={key} handleInput={handleInput} />
+    ))}
+  </div>
+)
+
 const App = () => {
-  const [display, setDisplay] = React.useState('0')
+  const [input, setInput] = React.useState('0')
+  const [output, setOutput] = React.useState('')
+  const [calculatorData, setCalculatorData] = React.useState('')
 
-  const handleClick = (e) => {
-    const $btn = document.querySelector(`.${e.target.className}`).innerText
-    const DONT_START_WITH_ZERO = /^0/g.test(display) || $btn === 0
-    const START_WITH_ZERO = !/^0/g.test(display)
-
-    console.log($btn)
-
-    if ($btn === '.' && $btn.length === 1) {
-      setDisplay('0.')
-    }
-
-    if (DONT_START_WITH_ZERO) {
-      setDisplay($btn)
-    } else if (START_WITH_ZERO) {
-      setDisplay(display.concat($btn))
+  const handleSubmit = () => {
+    if (!calculatorData.length) {
+      setInput(input)
+    } else {
+      const total = math.evaluate(calculatorData)
+      setInput(total)
+      setOutput(`${total} = ${total}`)
+      setCalculatorData(`${total}`)
     }
   }
 
-  //* EQUALS
-  const evaluateCalc = () => {
-    let newStr = display
-
-    if (/\++/g.test(display)) {
-      newStr = newStr.replaceAll(/\++/g, '+')
-    } else if (/\-+/g.test(display)) {
-      newStr = newStr.replaceAll(/\-+/g, '-')
-    } else if (/\*+/g.test(display)) {
-      newStr = newStr.replaceAll(/\*+/g, '*')
-    } else if (/\/+/g.test(display)) {
-      newStr = newStr.replaceAll(/\/+/g, '/')
-    }
-
-    if (/\.+/g.test(display)) {
-      newStr = newStr.replaceAll(/\.+/g, '.')
-    }
-
-    let result = math.evaluate(newStr).toString()
-
-    setDisplay(result)
+  const handleClear = () => {
+    setInput('0')
+    setCalculatorData('')
   }
 
-  //* CLEAR
-  const AC = () => setDisplay('0')
+  const handleNumbers = (value) => {
+    if (!calculatorData.length) {
+      setInput(`${value}`)
+      setCalculatorData(`${value}`)
+    } else {
+      if (value === 0 && (calculatorData === '0' || input === '0')) {
+        setCalculatorData(`${calculatorData}`)
+      } else {
+        const lastChat = calculatorData.at(-1)
+        const isLastChatOperator =
+          lastChat === '*' || operators.includes(lastChat)
+
+        setInput(isLastChatOperator ? `${value}` : `${input}${value}`)
+        setCalculatorData(`${calculatorData}${value}`)
+      }
+    }
+  }
+
+  const dotOperator = () => {
+    const lastChat = calculatorData.at(-1)
+    if (!calculatorData.length) {
+      setInput('0.')
+      setCalculatorData('0.')
+    } else {
+      if (lastChat === '*' || operators.includes(lastChat)) {
+        setInput('0.')
+        setCalculatorData(`${calculatorData} 0.`)
+      } else {
+        setInput(
+          lastChat === '.' || input.includes('.') ? `${input}` : `${input}.`
+        )
+        const formattedValue =
+          lastChat === '.' || input.includes('.')
+            ? `${calculatorData}`
+            : `${calculatorData}.`
+        setCalculatorData(formattedValue)
+      }
+    }
+  }
+
+  const handleOperators = (value) => {
+    if (calculatorData.length) {
+      setInput(`${value}`)
+      const beforeLastChat = calculatorData.at(-2)
+
+      const beforeLastChatIsOperator =
+        operators.includes(beforeLastChat) || beforeLastChat === '*'
+
+      const lastChat = calculatorData.at(-1)
+
+      const lastChatIsOperator =
+        operators.includes(lastChat) || lastChat === '*'
+
+      const validOp = value === 'x' ? '*' : value
+      if (
+        (lastChatIsOperator && value !== '-') ||
+        (beforeLastChatIsOperator && lastChatIsOperator)
+      ) {
+        if (beforeLastChatIsOperator) {
+          const updatedValue = `${calculatorData.substring(
+            0,
+            calculatorData.length - 2
+          )}${value}`
+          setCalculatorData(updatedValue)
+        } else {
+          setCalculatorData(
+            `${calculatorData.substring(
+              0,
+              calculatorData.length - 1
+            )}${validOp}`
+          )
+        }
+      } else {
+        setCalculatorData(`${calculatorData}${validOp}`)
+      }
+    }
+  }
+
+  const handleInput = (value) => {
+    const number = numbers.find((num) => num === value)
+    const operator = operators.find((op) => op === value)
+
+    switch (value) {
+      case '=':
+        handleSubmit()
+        break
+      case 'AC':
+        handleClear()
+        break
+      case number:
+        handleNumbers(value)
+        break
+      case '.':
+        dotOperator(value)
+        break
+      case operator:
+        handleOperators(value)
+        break
+      default:
+        break
+    }
+  }
+
+  const handleOutput = () => {
+    setOutput(calculatorData)
+  }
+
+  React.useEffect(() => {
+    handleOutput()
+  }, [calculatorData])
 
   return (
     <div className='container'>
-      <div id='display' className='btn-17'>
-        {display.replaceAll(/\.+/g, '.')}
+      <div className='calculator'>
+        <Display input={input} output={output} />
+        <Keyboard handleInput={handleInput} />
       </div>
-      <button id='zero' className='btn-0' onClick={handleClick}>
-        0
-      </button>
-      <button id='one' className='btn-1' onClick={handleClick}>
-        1
-      </button>
-      <button id='two' className='btn-2' onClick={handleClick}>
-        2
-      </button>
-      <button id='three' className='btn-3' onClick={handleClick}>
-        3
-      </button>
-      <button id='four' className='btn-4' onClick={handleClick}>
-        4
-      </button>
-      <button id='five' className='btn-5' onClick={handleClick}>
-        5
-      </button>
-      <button id='six' className='btn-6' onClick={handleClick}>
-        6
-      </button>
-      <button id='seven' className='btn-7' onClick={handleClick}>
-        7
-      </button>
-      <button id='eight' className='btn-8' onClick={handleClick}>
-        8
-      </button>
-      <button id='nine' className='btn-9' onClick={handleClick}>
-        9
-      </button>
-      <button id='decimal' className='btn-10' onClick={handleClick}>
-        .
-      </button>
-      <button id='add' className='btn-11' onClick={handleClick}>
-        +
-      </button>
-      <button id='subtract' className='btn-12' onClick={handleClick}>
-        -
-      </button>
-      <button id='multiply' className='btn-13' onClick={handleClick}>
-        *
-      </button>
-      <button id='divide' className='btn-14' onClick={handleClick}>
-        /
-      </button>
-      <button id='clear' className='btn-15' onClick={AC}>
-        AC
-      </button>
-      <button id='equals' className='btn-16' onClick={evaluateCalc}>
-        =
-      </button>
     </div>
   )
 }
