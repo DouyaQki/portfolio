1219431ae8f3864e51e7c6471c4897217b486354
DouyaQki <douyaqki@gmail.com>
2022-07-17 12:11:58 -0300

    freecodecamp projects

--- front_end_development_libraries/25_5_clock/index.jsx
+++ front_end_development_libraries/25_5_clock/index.jsx
@@ -42,6 +42,7 @@ const App = () => {
     const seconds = timeLeft - minutes * 60
     const formattedMinutes = minutes < 10 ? '0' + minutes : minutes
     const formattedSeconds = seconds < 10 ? '0' + seconds : seconds
+
     return `${formattedMinutes}:${formattedSeconds}`
   }
 
@@ -96,57 +97,61 @@ const App = () => {
   return (
     <div>
       <div className='wrapper'>
-        <h2>25 + 5 Clock</h2>
-        <div className='break-session-length'>
-          <div>
-            <h3 id='break-label'>Break Length</h3>
-            <div>
-              <button
-                disabled={play}
-                onClick={handleBreakIncrease}
-                id='break-increment'
-              >
-                Increase
-              </button>
-              <strong id='break-length'>{breakLength}</strong>
-              <button
-                disabled={play}
-                onClick={handleBreakDecrease}
-                id='break-decrement'
-              >
-                Decrease
-              </button>
-            </div>
+        <div className='break-session-box'>
+          <h3 id='break-label'>Break Length</h3>
+          <div className='break-session-box-buttons'>
+            <button
+              disabled={play}
+              onClick={handleBreakDecrease}
+              id='break-decrement'
+            >
+              -
+            </button>
+
+            <strong id='break-length'>{breakLength}</strong>
+            <button
+              disabled={play}
+              onClick={handleBreakIncrease}
+              id='break-increment'
+            >
+              +
+            </button>
           </div>
-          <div>
+        </div>
+
+        <div className='timer'>
+          <h2 id='timer-label'>{title}</h2>
+          <h3 id='time-left'>{timeFormatter()}</h3>
+        </div>
+
+        <div className='break-session-box'>
+          <div className='break-session-box'>
             <h3 id='session-label'>Session Length</h3>
-            <div>
-              <button
-                disabled={play}
-                onClick={handleSessionIncrease}
-                id='session-increment'
-              >
-                Increase
-              </button>
-              <strong id='session-length'>{sessionLength}</strong>
+            <div className='break-session-box-buttons'>
               <button
                 disabled={play}
                 onClick={handleSessionDecrease}
                 id='session-decrement'
               >
-                Decrease
+                -
+              </button>
+              <strong id='session-length'>{sessionLength}</strong>
+              <button
+                disabled={play}
+                onClick={handleSessionIncrease}
+                id='session-increment'
+              >
+                +
               </button>
             </div>
           </div>
         </div>
-        <div className='timer-wrapper'>
-          <div className='timer'>
-            <h2 id='timer-label'>{title}</h2>
-            <h3 id='time-left'>{timeFormatter()}</h3>
-          </div>
+
+        <div className='timer-buttons-box'>
           <button onClick={handlePlay} id='start_stop'>
-            Start/Stop
+          ⏵⏸
           </button>
+
           <button onClick={handleReset} id='reset'>
             Reset
           </button>
